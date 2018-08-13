4121925df7ea647bf59a3dd332bb85c65da61141
zDogemon <33721574+zDogemon@users.noreply.github.com>
2018-08-13 19:17:16 -0300

    hide keyboard and comments

--- js/main.js
+++ js/main.js
@@ -13,4 +13,16 @@ function choose() {
             h1.textContent = document.getElementById('opt2').value;
         }
     }
+    hideKeyboard(element);
+}
+
+function hideKeyboard(element) {
+    element.setAttribute('readonly', 'readonly'); // Force keyboard to hide on input field.
+    element.setAttribute('disabled', 'true'); // Force keyboard to hide on textarea field.
+    setTimeout(function() {
+        element.blur();  //actually close the keyboard
+        // Remove readonly attribute after keyboard is hidden.
+        element.removeAttribute('readonly');
+        element.removeAttribute('disabled');
+    }, 100);
 }
