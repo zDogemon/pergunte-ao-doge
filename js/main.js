40b6f120ff1f100213796a713f62aaacdca614fc
zDogemon <33721574+zDogemon@users.noreply.github.com>
2018-08-14 11:20:33 -0300

    v10

--- js/main.js
+++ js/main.js
@@ -16,23 +16,21 @@ function choose() {
         } else {
             doge += document.getElementById('opt2').value;
         }
+        
+        doge += '</h1>\
+                <a id="myButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%; margin-top: 30px" onclick="backMain()">Voltar</a>';
+    
+        document.getElementById('batata').innerHTML = doge;
     }
-    
-    doge += '</h1>\
-    <a id="myButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%; margin-top: 30px" onclick="backMain()">Voltar</a>';
-    
-    document.getElementById('batata').innerHTML = doge;
 }
 
 function backMain(){
     var html = '<h3>Pergunte ao doge</h3>\
         <div class="input-field col s12" style="width: 50%">\
-            <input id="opt1" type="text" onkeydown="if (event.keyCode == 13)\
-            document.getElementById("myButton").click()">\
+            <input id="opt1" type="text" onkeydown="if (event.keyCode == 13) document.getElementById("myButton").click();">\
             <label for="opt1">Opção 1</label>\
         </div>\
-        <div class="input-field col s12" style="width: 50%" onkeydown="if (event.keyCode == 13)\
-            document.getElementById("myButton").click()">\
+        <div class="input-field col s12" style="width: 50%" onkeydown="if (event.keyCode == 13) document.getElementById("myButton").click();">\
             <input id="opt2" type="text">\
             <label for="opt2">Opção 2</label>\
         </div>\
