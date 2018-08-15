422ca21206f480a4cf05732a7133ec2bc3a995f6
zDogemon <33721574+zDogemon@users.noreply.github.com>
2018-08-15 10:36:59 -0300

    v13

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
     backMain();
     $($("body").get(0)).children().get(1).remove();
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13)
+            $('#dogeButton').click();
+    });
 });
+
+
 
 function choose() {
     var y = Math.random();
@@ -15,17 +21,17 @@ function choose() {
         alert("Preencha todos os campos!");
     } else {
         if (opt1.val().toLowerCase().indexOf("crystian") > -1 && opt1.val().toLowerCase().indexOf("gay") > -1 && opt1.val().toLowerCase().indexOf("não") == -1) {
-            doge += opt1.val().toString();
+            doge += "Pergunta outra, essa é muito fácil";
         } else if (opt2.val().toLowerCase().indexOf("crystian") > -1 && opt2.val().toLowerCase().indexOf("gay") > -1 && opt2.val().toLowerCase().indexOf("não") == -1) {
-            doge += opt2.val().toString();
+            doge += "Pergunta outra, essa é muito fácil";
         } else if (y < 0.5) {
             doge += opt1.val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
         } else {
             doge += opt2.val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
         }
         doge += '</h1>\
-                <a id="myButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%; margin-top: 30px" onclick="backMain()">Voltar</a>';
-        
+                <a id="dogeButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%; margin-top: 24px" onclick="backMain()">Fazer outra pergunta</a>';
+
         $("#doge-content").html(doge);
     }
 }
@@ -33,14 +39,14 @@ function choose() {
 function backMain() {
     var html = '<h3>Pergunte ao doge</h3>\
         <div class="input-field col s12" style="width: 50%">\
-            <input id="opt1" type="text" onkeydown="if (event.keyCode == 13) document.getElementById(\'myButton\').click();">\
+            <input id="opt1" type="text">\
             <label for="opt1">Opção 1</label>\
         </div>\
-        <div class="input-field col s12" style="width: 50%" onkeydown="if (event.keyCode == 13) document.getElementById(\'myButton\').click();">\
+        <div class="input-field col s12" style="width: 50%">\
             <input id="opt2" type="text">\
             <label for="opt2">Opção 2</label>\
         </div>\
-        <a id="myButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%" onclick="choose()">Adivinhar</a>\
+        <a id="dogeButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%" onclick="choose()">Adivinhar</a>\
     ';
 
     $("#doge-content").html(html);
