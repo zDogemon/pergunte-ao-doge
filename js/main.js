769fe4ffa0f14cf292fe10ae7dedb7d8e5877b04
zDogemon <33721574+zDogemon@users.noreply.github.com>
2018-12-19 19:33:02 -0200

    Added dark theme

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-$(document).ready(function () {
+﻿$(document).ready(function () {
     backMain();
     $($("body").get(0)).children().get(1).remove();
     $(window).keydown(function (event) {
         if (event.keyCode == 13)
             $('#dogeButton').click();
+    });
+    $("#swdark").change(function (){
+    	if($(this).find("input").is(":checked")){
+            darkTheme();
+            document.cookie = "dark=true";
+        } else {
+            lightTheme();
+            document.cookie = "dark=false";
+        }
     });
 });
 
@@ -27,6 +36,10 @@ function choose() {
                 <a id="dogeButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%; margin-top: 24px" onclick="backMain()">Voltar</a>';
 
         $("#doge-content").html(doge);
+        
+        if($("#swdark").find("input").is(":checked")){
+            darkTheme();
+        }
     }
 }
 
@@ -40,8 +53,30 @@ function backMain() {
             <input id="opt2" type="text">\
             <label for="opt2">Opção 2</label>\
         </div>\
-        <a id="dogeButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%" onclick="choose()">Adivinhar</a>\
+        <a id="dogeButton" class="waves-effect waves-light btn blue darken-1" style="width: 50%" onclick="choose()">Perguntar</a>\
     ';
 
     $("#doge-content").html(html);
+
+    if($("#swdark").find("input").is(":checked")){
+        darkTheme();
+    }
+}
+
+function darkTheme(){
+    $("body").css("background-color", "#121212");
+    $("h1").css("color", "white");
+    $("h3").css("color", "white");
+    $("h4").css("color", "white");
+    $("textarea:focus, input:focus").css("color", "white");
+    $("input, select, textarea").css("color", "white");
+}
+
+function lightTheme(){
+    $("body").css("background-color", "Azure");
+    $("h1").css("color", "black");
+    $("h3").css("color", "black");
+    $("h4").css("color", "black");
+    $("textarea:focus, input:focus").css("color", "black");
+    $("input, select, textarea").css("color", "black");
 }
